fe6fa66ce1e05670c018e2ac6c034959d7e36d0b
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2025-07-15 02:38:48 -0700

    updated App path

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,7 @@ import GradeReport from './Components/Index/GradeReport'
 function App() {
    const router = createBrowserRouter(createRoutesFromElements(
       <>
-      <Route path='/frehiwots-portifolio' element={ <Index/> } /> 
-      <Route path='/practice' element={ <Practice/> } /> 
-      <Route path='/p5' element={<HerosectionBg/>}/>
-      <Route path='/grade-report' element={<GradeReport/>}/>
+      <Route path='/' element={ <Index/> } /> 
     </>
    ))
 
